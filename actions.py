bug fix: variance computation

actions.py
@@ -10,4 +10,4 @@ def ToyModelAction(fields, beta, conj=None):
 
     f1dagf2 = torch.sum(f1conj*f2, dim=-1)
     f2dagf1 = torch.sum(f2conj*f1, dim=-1)
-    return -beta * (f1dagf2 * f2dagf1).real
+    return -beta * (f1dagf2 * f2dagf1)

constant_deformations.py
@@ -1,5 +1,5 @@
 from utils import call_PDF
-from deformations import *
+import deformations as defs
 from actions import ToyModelAction
 from observables import OnePointFn
 import pdb
@@ -26,23 +26,23 @@ class VarianceLoss(nn.Module):
         self.obs = obs
         self.kwargs = kwargs
 
-        self.initial_var = obs(
-            func='var', model=self.model, sampletype="all", **self.kwargs).item()
-        self.target_exp = obs(
-            func='exp', model=self.model, sampletype="all", **self.kwargs).item()
+        self.initial_var = self.variance(deform=False, sampletype="all")
+        self.target_exp = self.expectation(deform=False, sampletype="all").real
 
-    def forward(self, alphas, **kwargs):
-        return self.variance(alphas, **kwargs)
-
-    def variance(self, alphas, batch_idx=None, sampletype="train"):
-        return self.obs(func='var', deform=True, alphas=alphas,
+    def get_obs(self, alphas=None, batch_idx=None,
+                deform=True, sampletype="train"):
+        return self.obs(deform=deform, alphas=alphas,
                         model=self.model, batch_idx=batch_idx,
                         sampletype=sampletype, **self.kwargs)
 
-    def expectation(self, alphas, batch_idx=None, sampletype="train"):
-        return self.obs(func='exp', deform=True, alphas=alphas,
-                        model=self.model, batch_idx=batch_idx,
-                        sampletype=sampletype, **self.kwargs)
+    def forward(self, **kwargs):
+        return self.variance(**kwargs)
+
+    def variance(self, **kwargs):
+        return self.get_obs(**kwargs).var()
+
+    def expectation(self, **kwargs):
+        return self.get_obs(**kwargs).mean()
 
 
 class Trainer:
@@ -66,7 +66,8 @@ class Trainer:
 
             batch_idx = torch.randperm(
                 self.model.samples["train"].size(0))[:self.batch_size]
-            loss = self.loss_fn(alphas, batch_idx=batch_idx)
+            loss = self.loss_fn(
+                alphas=alphas, batch_idx=batch_idx, sampletype="train")
             loss.backward()
             self.optimizer.step()
 
@@ -74,19 +75,24 @@ class Trainer:
                 print(f"Epoch {epoch}: Loss = {loss.item()}")
 
             alphas_copy = alphas.clone().detach()
-            self.train_var.append(loss.item())
-            self.train_exp.append(
-                self.loss_fn.expectation(alphas_copy, batch_idx=batch_idx))
-            self.test_var.append(
-                self.loss_fn.variance(alphas_copy, sampletype="test"))
-            self.test_exp.append(
-                self.loss_fn.expectation(alphas_copy, sampletype="test"))
             self.alpha_record.append(alphas_copy)
 
+            self.train_var.append(loss.item())
+            self.train_exp.append(self.loss_fn.expectation(
+                alphas=alphas_copy,
+                batch_idx=batch_idx,
+                sampletype="train"))
+            self.test_var.append(self.loss_fn.variance(
+                alphas=alphas_copy,
+                sampletype="test"))
+            self.test_exp.append(self.loss_fn.expectation(
+                alphas=alphas_copy,
+                sampletype="test"))
+
         self.train_var = torch.tensor(self.train_var)
-        self.train_exp = torch.tensor(self.train_exp)
+        self.train_exp = torch.tensor(self.train_exp).real
         self.test_var = torch.tensor(self.test_var)
-        self.test_exp = torch.tensor(self.test_exp)
+        self.test_exp = torch.tensor(self.test_exp).real
         self.alpha_record = torch.stack(self.alpha_record)
 
     def plot_training(self, fname='training.pdf', show=True,
@@ -102,7 +108,8 @@ class Trainer:
         ax[0].plot(range(self.epochs), self.test_exp,
                    label="test", c="tab:orange")
         ax[0].axhline(self.loss_fn.target_exp, c='k', label='undeformed')
-        ax[0].set_ylabel(r'$\mathtt{Exp}\left[Q_{'+sub_str+r'}\right]$')
+        ax[0].set_ylabel(
+            r'$\mathrm{Re}\left[\mathtt{Exp}Q_{'+sub_str+r'}\right]$')
 
         if plot_error:
             ax[0].fill_between(range(self.epochs),
@@ -149,12 +156,12 @@ class Trainer:
         call_PDF(fname, show=show)
 
 
-N = 2
+N = 3
 N_EPOCHS = 1000
 BETA = 4.5
 ACTION = ToyModelAction
 OBS = OnePointFn
-I, J, VARIDX = 0, 1, 1
+I, J, PIDX = 0, 1, 0
 BATCH_SIZE = 1000
 TRAINING_SPLIT = 0.8
 
@@ -169,7 +176,7 @@ if __name__ == "__main__":
     train_indices = torch.randperm(N_conf)[:int(TRAINING_SPLIT*N_conf)]
 
     N = samples.shape[-1] - 1
-    model = HomogenousDeformations(N, deftype="constant")
+    model = defs.HomogenousDeformations(N, deftype="constant")
     model.samples = {"all": samples.clone(),
                      "train": samples[train_indices].clone(),
                      "test": samples[~train_indices].clone()}
@@ -177,7 +184,7 @@ if __name__ == "__main__":
     obskwargs = {
         "i": I,
         "j": J,
-        "varidx": VARIDX,
+        "pidx": PIDX,
         "action": ACTION,
         "beta": BETA,
     }

deformations.py
@@ -89,10 +89,10 @@ class ProjectorDeformations(nn.Module):
         assert alphas.shape[-1] == self.DOF, "Alphas of incorrect size"
 
         B = alphas.view(*alphas.shape[:-1], self.size, self.size)
-        P = torch.einsum('...i,...j->...ij', X, X)
+        PX = torch.einsum('...i,...j->...ij', X, X)
         Id = self.eye.expand(*X.shape[:-1], self.size,
                              self.size) if X.dim() > 1 else self.eye
-        A = torch.einsum('...ij, ...jk->...ik', (Id-P), B)
+        A = torch.einsum('...ij, ...jk->...ik', (Id-PX), B)
         Y = torch.einsum('...ij,...j->...i', A, X)
         return Y
 
@@ -149,11 +149,11 @@ class TorusDeformations(nn.Module):
 
         alpha_H = torch.einsum(
             'i,ijk->jk', alphas.to(torch.complex128), self.basis)
-        Y = torch.einsum('ij,...i->...j', CNtoR2N(1j *
-                                                  alpha_H, matrix=True), X.to(torch.complex128))
+        Y = torch.einsum('ij,...i->...j', CNtoR2N(
+            1j * alpha_H, matrix=True), X.to(torch.complex128))
         return Y
 
-    def complexify(self, X, Y, alphas):
+    def complexify(self, X, Y):
         LX = X * Lambda(Y).unsqueeze(-1)
         Z = LX + 1j * Y
         return Z

general_deformations.py
@@ -8,7 +8,7 @@ import torch.optim as optim
 from tqdm import tqdm
 
 from actions import ToyModelAction
-from deformations import HomogenousDeformations
+import deformations as defs
 from observables import OnePointFn
 from plot_settings import plotparams
 from utils import call_PDF
@@ -44,23 +44,23 @@ class VarianceLoss(nn.Module):
         self.obs = obs
         self.kwargs = kwargs
 
-        self.initial_var = obs(
-            func='var', model=self.model, sampletype="all", **self.kwargs).item()
-        self.target_exp = obs(
-            func='exp', model=self.model, sampletype="all", **self.kwargs).item()
+        self.initial_var = self.variance(deform=False, sampletype="all")
+        self.target_exp = self.expectation(deform=False, sampletype="all").real
 
-    def forward(self, alphas, **kwargs):
-        return self.variance(alphas, **kwargs)
-
-    def variance(self, alphas, batch_idx=None, sampletype="train"):
-        return self.obs(func='var', deform=True, alphas=alphas,
+    def get_obs(self, alphas=None, batch_idx=None,
+                deform=True, sampletype="train"):
+        return self.obs(deform=deform, alphas=alphas,
                         model=self.model, batch_idx=batch_idx,
                         sampletype=sampletype, **self.kwargs)
 
-    def expectation(self, alphas, batch_idx=None, sampletype="train"):
-        return self.obs(func='exp', deform=True, alphas=alphas,
-                        model=self.model, batch_idx=batch_idx,
-                        sampletype=sampletype, **self.kwargs)
+    def forward(self, **kwargs):
+        return self.variance(**kwargs)
+
+    def variance(self, **kwargs):
+        return self.get_obs(**kwargs).var()
+
+    def expectation(self, **kwargs):
+        return self.get_obs(**kwargs).mean()
 
 
 class Trainer:
@@ -84,7 +84,8 @@ class Trainer:
 
             batch_idx = torch.randperm(
                 self.model.samples["train"].size(0))[:self.batch_size]
-            loss = self.loss_fn(self.parnet, batch_idx=batch_idx)
+            loss = self.loss_fn(alphas=self.parnet,
+                                batch_idx=batch_idx, sampletype="train")
             loss.backward()
             self.optimizer.step()
 
@@ -92,17 +93,21 @@ class Trainer:
                 print(f"Epoch {epoch}: Loss = {loss.item()}")
 
             self.train_var.append(loss.item())
-            self.train_exp.append(
-                self.loss_fn.expectation(self.parnet, batch_idx=batch_idx))
+            self.train_exp.append(self.loss_fn.expectation(
+                alphas=self.parnet,
+                batch_idx=batch_idx,
+                sampletype="train"))
             self.test_var.append(self.loss_fn.variance(
-                self.parnet, sampletype="test"))
+                alphas=self.parnet,
+                sampletype="test"))
             self.test_exp.append(self.loss_fn.expectation(
-                self.parnet, sampletype="test"))
+                alphas=self.parnet,
+                sampletype="test"))
 
         self.train_var = torch.tensor(self.train_var)
-        self.train_exp = torch.tensor(self.train_exp)
+        self.train_exp = torch.tensor(self.train_exp).real
         self.test_var = torch.tensor(self.test_var)
-        self.test_exp = torch.tensor(self.test_exp)
+        self.test_exp = torch.tensor(self.test_exp).real
 
     def plot_training(self, fname='training.pdf', show=True,
                       plot_error=True):
@@ -117,7 +122,8 @@ class Trainer:
         ax[0].plot(range(self.epochs), self.test_exp,
                    label="test", c="tab:orange")
         ax[0].axhline(self.loss_fn.target_exp, c='k', label='undeformed')
-        ax[0].set_ylabel(r'$\mathtt{Exp}\left[Q_{'+sub_str+r'}\right]$')
+        ax[0].set_ylabel(
+            r'$\mathrm{Re}\left[\mathtt{Exp}Q_{'+sub_str+r'}\right]$')
 
         if plot_error:
             ax[0].fill_between(range(self.epochs),
@@ -164,6 +170,7 @@ class Trainer:
         call_PDF(fname, show=show)
 
 
+N = 3
 N_EPOCHS = 450
 BETA = 4.5
 ACTION = ToyModelAction
@@ -174,7 +181,7 @@ TRAINING_SPLIT = 0.8
 
 if __name__ == "__main__":
 
-    file = h5py.File("CPN.h5", 'r')["configs"]
+    file = h5py.File(f"CP{N}.h5", 'r')["configs"]
     burn = int(file["info"]["burn"][0])
     stepsize = int(file["info"]["stepsize"][0])
     samples = torch.tensor(
@@ -185,7 +192,7 @@ if __name__ == "__main__":
     train_indices = torch.randperm(N_conf)[:int(TRAINING_SPLIT*N_conf)]
 
     N = samples.shape[-1] - 1
-    model = HomogenousDeformations(N, deftype="general")
+    model = defs.HomogenousDeformations(N, deftype="general")
     model.samples = {
         "all": samples,
         "train": samples[train_indices],
@@ -210,12 +217,13 @@ if __name__ == "__main__":
     trainer.train(update=False)
     print(f"{N_conf} configs, batch size {BATCH_SIZE}")
     vari, varf = loss_fn.initial_var, loss_fn.variance(
-        alphanet, sampletype="all")
+        alphas=alphanet, sampletype="all")
     print(f"Variance reduction {vari} -> {varf} ({
           (vari-varf)*100/vari}%)")
 
     stni = loss_fn.target_exp/(vari**0.5)
-    stnf = loss_fn.expectation(alphanet, sampletype="all")/(varf**0.5)
+    stnf = loss_fn.expectation(
+        alphas=alphanet, sampletype="all").real/(varf**0.5)
     print(f"StN improvement {stni} -> {stnf} ({
           (stnf-stni)*100/stni}%)")
     trainer.plot_training()

observables.py
@@ -2,15 +2,14 @@ import torch
 import pdb
 
 from actions import ToyModelAction
-from utils import CNtoR2N
+from utils import CNtoR2N, Lambda
 
 
-def OnePointFn(i=0, j=0, action=ToyModelAction, func='var', varidx=0,
+def OnePointFn(i=0, j=0, action=ToyModelAction, pidx=0,
                deform=False, model=None, alphas=None, beta=4.5,
                batch_idx=slice(None), sampletype="train",
                pause=False, **kwargs):
 
-    assert func in ["var", "exp"], "func must be exp or var"
     assert sampletype in ["train", "test",
                           "all"], "sampletype must be train, test or all"
 
@@ -26,22 +25,15 @@ def OnePointFn(i=0, j=0, action=ToyModelAction, func='var', varidx=0,
         z = Z[..., ::2] + 1j * Z[..., 1::2]
         zconj = Z[..., ::2] - 1j * Z[..., 1::2]
 
-        obs = z[:, varidx, i-1] * zconj[:, varidx, j-1]
+        obs = z[:, pidx, i] * zconj[:, pidx, j]
         Sdiff = torch.exp(-action(z, beta, conj=zconj) + action(x, beta))
         detJ = model.detJac(X, Y, alphas)
 
         obs *= Sdiff*detJ
-        if func == "var":
-            obs *= zconj[:, varidx, i-1] * z[:, varidx, j-1]
-            # conjugate of Sdiff should be Sdiff (all real?)
-            obs *= Sdiff*detJ.conj()
-
     else:
-        obs = x.conj()[:, varidx, i-1] * \
-            x[:, varidx, j-1]
-        if func == "var":
-            obs *= obs.conj()
+        obs = x[:, pidx, i] * x.conj()[:, pidx, j]
 
     if pause:
         pdb.set_trace()
-    return torch.mean(obs).real
+
+    return obs